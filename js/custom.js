bcf00654cd0eb057c47cb99f79b419b99937ab3f
akshara <banoth.akshara.bai@gmail.com>
2025-01-08 14:51:50 +0530

    change

--- js/custom.js
+++ js/custom.js
@@ -9,7 +9,7 @@ getYear();
 
 // overlay menu
 function openNav() {
-    document.getElementById("myNav").classList.toggle("menu_width");
+    document.getElementById("akshara").classList.toggle("menu_width");
     document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
 }
 
